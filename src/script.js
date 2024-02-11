ffd9f06ed6a1db918bde0e97c4d550d039510cd6
AboubakarArisar <aboubakar.bscsf22@iba-suk.edu.pk>
2024-02-12 00:45:31 +0500

    animation added

--- src/script.js
+++ src/script.js
@@ -2,3 +2,26 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+
+let container = document.querySelector('#box-container');
+
+let image = document.querySelector('#fixed-image');
+
+container.addEventListener("mouseenter", function () {
+    image.style.display = "block";
+})
+
+
+container.addEventListener("mouseleave", function () {
+    image.style.display = "none";
+})
+
+let boxes = document.querySelectorAll('#box');
+
+boxes.forEach((e) => {
+    e.addEventListener("mouseenter", function () {
+        let img = e.getAttribute("data-image");
+        image.style.backgroundImage = `url(${img})`;  
+    })
+})
